6d3542899ea9cd3005bec3825d51330917eebe74
Aidan Payne <Terlen@users.noreply.github.com>
2018-04-29 20:18:05 -0700

    Create SQL Script to insert data in tables

---
+++ dataInsert.sql
@@ -0,0 +1,9 @@
+use footballLeague;
+
+INSERT INTO Team
+VALUES ('Sun Devils', 'Tempe', 'AZ'),('Buffalos', 'Boulder', 'CO'),('Aggies', 'Los Cruces', 'NM'),
+		('Ducks', 'Eugene', 'OR'), ('Beavers', 'Corvallis', 'OR'),('Aztecs', 'San Diego', 'CA'),
+		('Cardinals','Stanford','CA'),('Red Raiders', 'Lubboch','TX'),('Bruins','Passadena','CA'),
+		('Wildcats','Tuscon','AZ'),('Trojans','Los Angeles','CA'),('Utes','Salt Lake City','UT'),
+		('Huskies','Seattle','WA')
+
