3d333ba4c50f214c18682f787b0e6175d115790b
Aidan Payne <Terlen@users.noreply.github.com>
2018-04-29 21:58:47 -0700

    Increased data insert, added Date to Matchup table

--- dataInsert.sql
+++ dataInsert.sql
@@ -7,3 +7,24 @@ VALUES ('Sun Devils', 'Tempe', 'AZ'),('Buffalos', 'Boulder', 'CO'),('Aggies', 'L
 		('Wildcats','Tuscon','AZ'),('Trojans','Los Angeles','CA'),('Utes','Salt Lake City','UT'),
 		('Huskies','Seattle','WA')
 
+INSERT INTO Stadium
+VALUES ('Sun Devil','Tempe','AZ'),('Folsom Field','Boulder','CO'),('Aggie Memorial','Los Cruces','NM'),
+		('Autzen','Eugene','OR'),('Reser','Corvallis','OR'),('San Diego State','San Diego','CA'),
+		('Stanford','Stanford','CA'),('Jones AT&T','Lubboch','TX'),('Rose Bowl','Pasadena','CA'),
+		('Arizona','Tuscon','AZ'),('Los Angeles Memorial','Los Angeles','CA'),('Rice-Eccles','Salt Lake City','UT'),
+		('Husky','Seattle','WA')
+
+INSERT INTO Player
+VALUES ('A.J.','McCollum',NULL,74,311,'1/1/1990'),('Abe','Thompson',NULL,74,230,'1/1/1990'),
+		('Alani','Latu',NULL,74,248,'1/1/1990'),('Alex','Losoya',NULL,76,290,'1/1/1990'),
+		('Alex','Perry',NULL,71,169,'1/1/1990'),('Alexander','Otero',NULL,76,263,'1/1/1990')
+
+SELECT * FROM Matchup
+
+INSERT INTO Matchup
+VALUES (1,1,3,'08/31/17'),(1,1,6,'09/01/17'),(8,8,1,'09/16/17'),(1,1,4,'09/23/17'),(7,7,1,'09/30/17'),
+		(1,1,13,'10/14/17'),(12,12,1,'10/21/17'),(1,1,11,'10/28/17'),(1,1,2,'11/04/17'),(9,9,1,'11/11/17'),
+		(5,5,1,'11/18/17'),(1,1,10,'11/25/17')
+
+
+
